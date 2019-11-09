cb15c2d3f889927fbdd4c9d4b7890d8e8cc67c82
Joe Thompson <joe@pop-os.localdomain>
2019-11-08 19:31:07 -0500

    Completed stage one for login component and local storage for returened token

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "../utils/api";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+    const [user, setUser] = useState({
+        username: "",
+        password: ""
+    });
+
+    const changeHandler = (event) => {
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        })
+    };
+
+    const submitHandler = (event) => {
+            event.preventDefault();
+
+            api()
+                .post("/api/login", user)
+                .then(result => {
+                    console.log(result);
+                    localStorage.setItem("token", result.data.payload);
+                    props.history.push("/bubbles")
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+        <form onSubmit={submitHandler}>
+            <input type="text" name="username" placeholder="Username" value={user.username} onChange={changeHandler} />
+            <input type="password" name="password" placeholder="Password" value={user.password} onChange={changeHandler} />
+            <button type="submit">Log In</button>
+        </form>
     </>
   );
 };
